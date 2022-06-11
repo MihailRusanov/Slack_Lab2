2014a92d03337961d3423069c89e76b356f8a062
MihailRusanov <69981178+MihailRusanov@users.noreply.github.com>
2022-06-11 15:27:38 +0300

    add new function
    added new function for get a uniq numbers

---
+++ quest1_v0.0.2.py
@@ -0,0 +1,13 @@
+numbers = list(map(int, input().split()))
+# numbers = [1, 2, 3, 1, 2, 5, 6, 7, 10, 10, 0, -4, -3, 12, 10]
+# numbers.sort()
+
+
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
